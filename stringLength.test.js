11327c0292919e7ef5b9f5740f3c55c635705660
sainingo <nickmentor421@gmail.com>
2022-04-06 00:17:39 +0300

    reverse a tring method implemented

--- stringLength.test.js
+++ stringLength.test.js
@@ -1,7 +1,28 @@
- const { expect } = require('@jest/globals');
+//  const { expect } = require('@jest/globals');
+// const { describe } = require('yargs');
 const stringLength = require('./stringLength');
+const reverseString = require('./reverseString');
 
-test('expect output to be reverse', () => {
-  const reversedStr = stringLength('hello');
-  expect(reversedStr).toBe(5)
-});
+describe('outer', () => {
+    test('expect output character count to be 5 & more than 1', () => {
+        const reversedStr = stringLength('hello');
+        expect(reversedStr).toBe(5)
+      });
+      
+      test('should be longer less than 10', () => {
+          const strLength = stringLength('hellohellohello');
+          expect(strLength).toMatch('Error');
+      });
+      
+      test('should be longer than 1', () => {
+          const strLength = stringLength('h');
+          expect(strLength).toMatch('Count > 1');
+      });
+})
+
+describe('Reverse', () => {
+    test('should be a reverse str', () => {
+        const reverseStr = reverseString('hello');
+        expect(reverseStr).toMatch('olleh');
+    });
+})
